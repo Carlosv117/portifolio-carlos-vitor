e05b1cb2f55f6a167b061b879fb83480af202a97
Carlos Vitor <limacarlosvitor@gmail.com>
2022-06-30 13:54:34 -0300

    feat: creating footer

---
+++ src/Components/Footer/styles.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const FooteR = styled.footer`
+    background-color: rgb(13, 17, 27);
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    #icons{
+        display: flex;
+        align-items: center;
+        padding-bottom: 20px;
+
+        figure{
+            width: 30px;
+
+            img{
+                width: 100%;
+            }
+        }
+
+        h2{
+            padding: 0 30px;
+        }
+    }
+
+    div{
+        
+        p{
+            text-align: center;
+        }
+    }
+`
