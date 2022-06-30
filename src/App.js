2b43f649688ba4c636e2b576cd7a0d1db8389fd0
Carlos Vitor <limacarlosvitor@gmail.com>
2022-06-30 19:16:12 -0300

    feat: adding toast library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { RoutesPages } from './Routers';
 import GlobalStyle from './Styles/global';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
       <GlobalStyle/>
       <RoutesPages/>
+      <ToastContainer/>
     </>
   );
 }
