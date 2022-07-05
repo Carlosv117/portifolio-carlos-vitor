e9e18aeeda0d48a0e4ccab6c8ad10055a0c66ea8
Carlos Vitor <limacarlosvitor@gmail.com>
2022-07-05 02:35:24 -0300

    feat: adding new project and creating a database

---
+++ src/db/index.js
@@ -0,0 +1,67 @@
+import uBeer from "../img/projects/uBeer.png"
+import OnganizerApi from "../img/projects/Onganizer-api.png"
+import KenzieHub from "../img/projects/KenzieHub.png"
+import NuKenzie from "../img/projects/NuKenzie.png"
+import BurguerKenzie from "../img/projects/BurguerKenzie.png"
+import KenziePet from "../img/projects/KenziePet.png"
+import KenzieFood from "../img/projects/KenzieFood.png"
+import KMDB from "../img/projects/KMDB.png"
+
+export const listProjects = [
+    {
+        name: "uBeer",
+        img: `${uBeer}`,
+        category: "Full-stack",
+        description: "Aplicação web para buscar motoristas que sairam com o próprio carro e acabaram ingerindo bebidas alcoólicas",
+        link: "https://github.com/jrpolesi/ubeer",
+    },
+    {
+        name: "Onganizer Api",
+        img: `${OnganizerApi}`,
+        category: "Back-end",
+        description: "Uma API desenvolvida para facilitar as pessoas a fazerem doações e receberem as doações.",
+        link: "https://github.com/Carlosv117/onganizer-api",
+    },
+    {
+        name: "Kenzie Hub",
+        img: `${KenzieHub}`,
+        category: "Front-end",
+        description: "É uma plataforma onde você consegue botar as tecnologias que você está aprendendo ou já tem um certo nível de conhecimento.",
+        link: "https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-s2-kenzie-hub-carlosv117",
+    },
+    {
+        name: "Nu Kenzie",
+        img: `${NuKenzie}`,
+        category: "Front-end",
+        description: "Aplicação web para monitorar os gastos e custos da sua conta bancaria.",
+        link: "https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-s1-nu-kenzie-carlosv117",
+    },
+    {
+        name: "KMDB",
+        img: `${KMDB}`,
+        category: "Back-end",
+        description: "Uma API desenvolvida para organizar os usuários, filmes, gêneros dos filmes e as criticas dos usuários.",
+        link: "https://github.com/Kenzie-Academy-Brasil-Developers/m5-sprint4-kmdb-Carlosv117",
+    },
+    {
+        name: "Burguer Kenzie",
+        img: `${BurguerKenzie}`,
+        category: "Front-end",
+        description: "Hamburgueria feita totalmente em ReactJs e buscando requisição na API de produtos.",
+        link: "https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-s1-hamburgueria-da-kenzie-carlosv117",
+    },
+    {
+        name: "Kenzie Food",
+        img: `${KenzieFood}`,
+        category: "Front-end",
+        description: "Um projeto de um ecomerce utilizando Html, Css, Javascript (POO e API)",
+        link: "https://github.com/Kenzie-Academy-Brasil-Developers/api-kenziefood-m2-felipesindeaux",
+    },
+    {
+        name: "Kenzie Pet",
+        img: `${KenziePet}`,
+        category: "Back-end",
+        description: "Uma API desenvolvida para organizar os animais, o grupo de cada animal e as características de cada animal.",
+        link: "https://github.com/Kenzie-Academy-Brasil-Developers/m5-sprint3-kenzie-pet-Carlosv117",
+    },
+]
