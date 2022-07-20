e67fbf8a79404f0f1c2afcc4b49779dcd515b4cf
Carlos Vitor <limacarlosvitor@gmail.com>
2022-07-19 21:10:36 -0300

    feat: creating animated particles background

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { RoutesPages } from './Routers';
 import GlobalStyle from './Styles/global';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Particle from './Components/Particles';
 
 function App() {
   return (
     <>
+      <Particle/>
       <GlobalStyle/>
       <RoutesPages/>
       <ToastContainer/>
