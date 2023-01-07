ae763d9e41fd529dd2242c57d16a6bb1343b9440
carlos.lima <carlos.lima@impar.com.br>
2023-01-07 11:22:47 -0300

    resume updates

--- src/Components/CarroselCertificate/index.jsx
+++ src/Components/CarroselCertificate/index.jsx
@@ -1,27 +1,34 @@
 import React, { useEffect } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
-import frontend from "../../img/certificate/front-end-kenzie.png"
-import git from "../../img/certificate/git.jpg"
-import Aos from "aos"
-import "aos/dist/aos.css"
+import { Carousel } from "react-responsive-carousel";
+import frontend from "../../img/certificate/front-end-kenzie.png";
+import fullstack from "../../img/certificate/full-stack.png";
+import WebModerno from "../../img/certificate/WebModerno.png";
+import git from "../../img/certificate/git.jpg";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 export default function CarroselCertificate() {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
 
-    useEffect(() => {
-        Aos.init({duration: 2000})
-    }, [])
-    
-    return (
-        <div data-aos="zoom-in">
-            <Carousel infiniteLoop useKeyboardArrows autoPlay showThumbs={false}>
-                <div>
-                    <img src={frontend} alt="certificado" />
-                </div>
-                <div>
-                    <img src={git} alt="certificado" />
-                </div>
-            </Carousel>
+  return (
+    <div data-aos="zoom-in">
+      <Carousel infiniteLoop useKeyboardArrows autoPlay showThumbs={false}>
+        <div>
+          <img src={fullstack} alt="certificado" />
         </div>
-    );
+        <div>
+          <img src={frontend} alt="certificado" />
+        </div>
+        <div>
+          <img src={WebModerno} alt="certificado" />
+        </div>
+        <div>
+          <img src={git} alt="certificado" />
+        </div>
+      </Carousel>
+    </div>
+  );
 }
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,151 +1,173 @@
-import React, { useEffect, useState } from "react"
-import Header from "../../Components/Header"
-import { Main } from "./styles"
-import TypeIt from "typeit-react"
-import background from "../../img/backgrounds/background.mp4"
-import space from "../../img/backgrounds/spaceLogo.mp4"
-import cel from "../../img/icon/Cel.png"
-import github from "../../img/icon/github (1).png"
-import linkedin from "../../img/icon/linkedin (1).png"
-import front from "../../img/icon/front.png"
-import back from "../../img/icon/back.png"
-import Footer from "../../Components/Footer"
-import CarroselCertificate from "../../Components/CarroselCertificate"
-import { Contact } from "../../Components/Form"
-import Aos from "aos"
-import "aos/dist/aos.css"
-import Projects from "../../Components/ListProjects"
-import { listProjects } from "../../db"
+import React, { useEffect, useState } from "react";
+import Header from "../../Components/Header";
+import { Main } from "./styles";
+import TypeIt from "typeit-react";
+import background from "../../img/backgrounds/background.mp4";
+import space from "../../img/backgrounds/spaceLogo.mp4";
+import cel from "../../img/icon/Cel.png";
+import github from "../../img/icon/github (1).png";
+import linkedin from "../../img/icon/linkedin (1).png";
+import front from "../../img/icon/front.png";
+import back from "../../img/icon/back.png";
+import Footer from "../../Components/Footer";
+import CarroselCertificate from "../../Components/CarroselCertificate";
+import { Contact } from "../../Components/Form";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import Projects from "../../Components/ListProjects";
+import { listProjects } from "../../db";
 
 const Home = () => {
+  const [list, setList] = useState([]);
 
-    const [list, setList] = useState([])
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+    setList(listProjects);
+  }, []);
 
-    useEffect(() => {
-        Aos.init({duration: 2000})
-        setList(listProjects)
-    }, [])
+  return (
+    <>
+      <Header />
+      <Main>
+        <section className="home">
+          <video id="back" src={background} autoPlay loop muted />
+          <div>
+            <div id="text" data-aos="fade-down">
+              <div className="h1">
+                <h2>Carlos Vitor Cerqueira</h2>
+              </div>
+              <TypeIt
+                options={{ loop: true }}
+                getBeforeInit={(instance) => {
+                  instance
+                    .type("Desenvolvedor Front-end.")
+                    .pause(1000)
+                    .delete(24)
+                    .type("Desenvolvedor Back-end.")
+                    .pause(1000)
+                    .delete(23)
+                    .type("Desenvolvedor Full-stack.")
+                    .pause(1000)
+                    .delete(25);
+                  return instance;
+                }}
+              />
+            </div>
+            <div id="phone" data-aos="fade-up">
+              <figure>
+                <img src={cel} alt="icon phone" />
+              </figure>
+              <div>
+                <figure>
+                  <a
+                    href="https://github.com/Carlosv117"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <img src={github} alt="icon github" />
+                  </a>
+                </figure>
+                <figure>
+                  <a
+                    href="https://www.linkedin.com/in/carlosvitorcerqueira"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <img src={linkedin} alt="icon linkedin" />
+                  </a>
+                </figure>
+              </div>
+            </div>
+          </div>
+        </section>
+        <div>
+          <section className="about">
+            <div className="h1">
+              <h1>Sobre</h1>
+            </div>
+            <div>
+              <div id="video" data-aos="fade-right">
+                <video id="back" src={space} autoPlay loop muted />
+              </div>
+              <div data-aos="zoom-in">
+                <h2>Carlos Vitor Cerqueira</h2>
+                <p>
+                  Desenvolvedor Full Stack (+1 ano)
+                  <br />
+                  Trabalhei em vários projetos como Desenvolvedor, sendo que em
+                  alguns deles eu fui o principal responsável.
+                  <br />
+                  Sempre interessado em criar um código muito compreensível e
+                  funcional.
+                  <br />
+                  Apaixonado por aprender novas tecnologias e trabalhos
+                  desafiadores, assim como ensinar e ajudar minha equipe.
+                </p>
+                <a
+                  href="https://drive.google.com/file/d/1W2CWcjlp0b48a8g8IcCRzKip5GQLnjnx/view?usp=share_link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Curriculo
+                </a>
+              </div>
+            </div>
+          </section>
+          <section className="tecnologies">
+            <div className="h1">
+              <h1>Tecnologias</h1>
+            </div>
+            <div>
+              <div data-aos="flip-right">
+                <figure>
+                  <img src={front} alt="icon front-end" />
+                </figure>
+                <h3>Front-end</h3>
+                <span>React</span>
+                <span>Javascript</span>
+                <span>Typescript</span>
+                <span>API REST</span>
+                <span>Logic Apps</span>
+                <span>SPFx (Sharepoint Framework)</span>
+              </div>
+              <div data-aos="flip-left">
+                <figure>
+                  <img src={back} alt="icon back-end" />
+                </figure>
+                <h3>Back-end</h3>
+                <span>Node</span>
+                <span>Python</span>
+                <span>Django</span>
+                <span>Docker</span>
+                <span>Express</span>
+                <span>PostgreSQL</span>
+              </div>
+            </div>
+          </section>
+          <section className="qualifications">
+            <div className="h1">
+              <h1>Qualificações</h1>
+            </div>
+            <CarroselCertificate />
+          </section>
+          <section className="projects">
+            <div className="h1">
+              <h1>Projetos</h1>
+            </div>
+            <Projects list={list} />
+          </section>
+        </div>
+        <section className="contact">
+          <div className="h1">
+            <h1>Contato</h1>
+          </div>
+          <video id="backContact" src={background} autoPlay loop muted />
+          <Contact />
+        </section>
+      </Main>
+      <Footer />
+    </>
+  );
+};
 
-    
-    return(
-        <>
-            <Header/>
-            <Main>
-                <section className="home">
-                        <video id="back" src={background} autoPlay loop muted/>
-                        <div>
-                            <div id="text" data-aos="fade-down">
-                                <div className="h1">
-                                    <h2>Carlos Vitor Cerqueira</h2>
-                                </div>
-                                <TypeIt
-                                    options={{loop: true}}
-                                    getBeforeInit={(instance) => {
-                                        instance.type("Desenvolvedor Front-end.")
-                                        .pause(1000).delete(24)
-                                        .type("Desenvolvedor Back-end.")
-                                        .pause(1000).delete(23)
-                                        .type("Desenvolvedor Full-stack.")
-                                        .pause(1000).delete(25)
-                                        return instance
-                                    }}
-                                />
-                            </div>
-                            <div id="phone" data-aos="fade-up">
-                                <figure>
-                                    <img src={cel} alt="icon phone"/>
-                                </figure>
-                                <div>
-                                    <figure>
-                                        <a href="https://github.com/Carlosv117" rel="noreferrer" target="_blank">
-                                            <img src={github} alt="icon github"/>
-                                        </a>
-                                    </figure>
-                                    <figure>
-                                        <a href="https://www.linkedin.com/in/carlosvitorcerqueira" rel="noreferrer" target="_blank">
-                                            <img src={linkedin} alt="icon linkedin"/>
-                                        </a>
-                                    </figure>
-                                </div>
-                            </div>
-                        </div>
-                </section>
-                <div>
-                    <section className="about">
-                        <div className="h1">
-                            <h1>Sobre</h1>
-                        </div>
-                        <div>
-                            <div id="video" data-aos="fade-right">
-                                <video id="back" src={space} autoPlay loop muted/>
-                            </div>
-                            <div data-aos="zoom-in">
-                                <h2>Carlos Vitor Cerqueira</h2>
-                                <p>Estou em busca de um emprego na área de Desenvolvedor e, 
-                                    através disso, complementar o que venho aprendendo e evoluir 
-                                    na empresa em que estiver atuando e contribuir o máximo possível. 
-                                    Sou desenvolvedor Full-stack formado pela Kenzie Academy Brasil.</p>
-                                <a href="https://drive.google.com/file/d/1g1MA5Hm6AD3J7koxKlKQypQ49JkwcuDO/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-                                    Curriculo
-                                </a>
-                            </div>
-                        </div>
-                    </section>
-                    <section className="tecnologies">
-                        <div className="h1">
-                            <h1>Tecnologias</h1>
-                        </div>
-                        <div>
-                            <div data-aos="flip-right">
-                                <figure>
-                                    <img src={front} alt="icon front-end"/>
-                                </figure>
-                                <h3>Front-end</h3>
-                                <span>React</span>
-                                <span>Javascript</span>
-                                <span>Typescript</span>
-                                <span>API REST</span>
-                                <span>HTML5</span>
-                                <span>CSS3</span>
-                            </div>
-                            <div data-aos="flip-left">
-                                <figure>
-                                    <img src={back} alt="icon back-end"/>
-                                </figure>
-                                <h3>Back-end</h3>
-                                <span>Python</span>
-                                <span>Node</span>
-                                <span>Express</span>
-                                <span>Django</span>
-                                <span>PostgreSQL</span>
-                                <span>Docker</span>
-                            </div>
-                        </div>
-                    </section>
-                    <section className="qualifications">
-                        <div className="h1">
-                            <h1>Qualificações</h1>
-                        </div>
-                        <CarroselCertificate/>
-                    </section>
-                    <section className="projects">
-                        <div className="h1">
-                            <h1>Projetos</h1>
-                        </div>
-                        <Projects list={list}/>
-                    </section>
-                </div>
-                <section className="contact">
-                    <div className="h1">
-                        <h1>Contato</h1>
-                    </div>
-                    <video id="backContact" src={background} autoPlay loop muted/>
-                    <Contact/>
-                </section>
-            </Main>
-            <Footer/>
-        </>
-    )
-}
-
-export default Home
+export default Home;
